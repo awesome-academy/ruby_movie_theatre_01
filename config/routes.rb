0d32ff181bc2143bada0ed65279f3ee3bd8cb88a
vietcrit96 <vietcrit@gmail.com>
2019-02-07 10:43:52 +0700

    Ref task #6007 Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
     get "static_pages/*page" => "static_pages#show"
+    get "/signup", to: "user#new"
+    post "/signup", to: "user#create"
+    resources :user
   end
 end
