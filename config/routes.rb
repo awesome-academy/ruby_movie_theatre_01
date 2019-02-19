d42830098b5b3084c6a2e08ba2e5adbd94cf6e9b
vietcrit96 <vietcrit@gmail.com>
2019-02-19 10:10:01 +0700

    Ref task 6650 User and admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
     get "static_pages/*page" => "static_pages#show"
-    get "/signup", to: "user#new"
-    post "/signup",  to: "user#create"
+    get "/signup", to: "users#new"
+    post "/signup",  to: "users#create"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout",  to: "sessions#destroy"
-    resources :user
+    resources :users
   end
 end
